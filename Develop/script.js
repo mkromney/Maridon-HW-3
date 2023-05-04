c169530012e023e1e3f95622bc20ed1466e7b39f
Maridon Romney <mkromney@gmail.com>
2023-05-03 22:01:14 -0600

    Added some very important notes to the code - detailed research informed how I used the code and the notes helped me to decode their meaning and usage in a way I can use later.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,9 @@
-
-
-
-
-// Define a function to generate a password based on user-selected criteria
+// We begin with an empty function that will define a password based on the attributes that follow it //
 function generatePassword() {
-  // Prompt the user for the desired length of the password
+  // parseInt takes an argument and turns it into a string that returns a value, in this case we need to ask it to define the length of the password, the parameters are set below //
   var passwordLength = parseInt(prompt("Enter the desired length of your password (between 8 and 128 characters):"));
 
-  // Validate the password length input
+  // Here we have ensureed that the password will not exceed nor fall below a certain length based on the criteria I set - not less than 8 and not more than 128, the while function logs this and returns an error if a given number falls outside these parameters //
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     passwordLength = parseInt(prompt("Invalid input! Please enter a number between 8 and 128:"));
   }
@@ -23,10 +19,8 @@ function generatePassword() {
   var numberChars = "0123456789";
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  
-  
 
-  // We need to have an open variable not yet defined by the parameters a user will be able to define //
+  // We need to have an open variable not yet defined by the parameters - a user will later define //
   var password = "";
 
   // This for loop considers the user's input selections and uses if statements to decide whether or not to use a character type in the password. It considers the password length the user stored under the passwordLength var //
@@ -38,10 +32,9 @@ function generatePassword() {
     if (includeNumbers) charType += numberChars;
     if (includeSpecialChars) charType += specialChars;
 
-    // This line was particularly difficult to figure out. 
+    // This line was particularly difficult to figure out. The variable randomChar is determined by: math.floor which "rounds down and returns the largest integer less than or equal to a given number. math.random returns a double value with a positive sign, greater than or equal to 0.0 and less than 1.0 - essentially we are determining the range of possibilities, and making sure that while they are random, they fall within in the parameters we have set, from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor - and: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random - this code also references the length we set in previous lines under passwordLength //
     var randomChar = charType.charAt(Math.floor(Math.random() * charType.length));
     
-    // Add the randomly chosen character to the password 
     // This bit will add the user's chosen character types. The '+=' operator I use above and below is known as the addition assignment and it performs an addition - usually a string concatenation - and assigns the result to the left operand. Learned from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment //
     password += randomChar;
   }
@@ -50,8 +43,7 @@ function generatePassword() {
   return password;
 }
 
-// Write password to the #password input
-// Define a function to write the generated password to the HTML page //
+// This function's task is to write the password. This is linked to the box in the HTML script and allows us to see the coded responses of our queries. The hashtag # in #password is an ID attribute of the linked HTML element, using the query.selector as a criteria we can pull the first element that matches the parameters given in the code above //
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -61,6 +53,6 @@ function writePassword() {
 
 
 // Add an event listener to the "Generate Password" button to call the writePassword function //
-// Add event listener to generate button - "this is a way that you can wait for user interaction like a click or keypress and then run some code whenevber that action happens"- definition from https://blog.webdevsimplified.com/2022-01/event-listeners/#:~:text=An%20event%20listener%20in%20JavaScript,click%20events%20on%20a%20button. //
+// Adds an event listener to generate button - "this is a way that you can wait for user interaction like a click or keypress and then run some code whenever that action happens"- definition from https://blog.webdevsimplified.com/2022-01/event-listeners/#:~:text=An%20event%20listener%20in%20JavaScript,click%20events%20on%20a%20button. //
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
